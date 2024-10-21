ea437e4478ae9be655db72e3069eddb11ae08fa5
Aysel Bilmez <aybilmez@gmail.com>
2024-10-22 00:58:54 +0300

    Proje dosyası ekle.

---
+++ PatikaW3P1_IlkSinifimiziOlusturuyoruz/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaW3P1_IlkSinifimiziOlusturuyoruz
+{
+    public class Person // person adinda bir public class olusturuyoruz
+    {
+        public string ad; // degiskenleri public bir sekilde tanimliyoruz
+        public string soyad;
+        public string dogumTarihi;
+
+        public string Ad // degisken kullanildiginda nasil bir yol izlenecek onu veriyoruz
+        {
+            get
+            {
+                return ad; //ad degiskeni geri donecek ve set ile ad verilen degere atanacak
+            }
+            set
+            {
+                ad = value;
+            }
+        }
+        public string Soyad
+        {
+            get
+            {
+                return soyad;
+            }
+            set
+            {
+                soyad = value;
+            }
+        }
+        public string DogumTarihi
+        {
+            get
+            {
+                return dogumTarihi;
+            }
+            set
+            {
+                dogumTarihi = value;
+            }
+        }
+
+        public void OgrenciyiYazdir() //metodu ogrenciyse kullaniyoruz ve konsola yazma islevini veriyor
+        {
+            Console.WriteLine($"Ogrencinin adi {Ad} {Soyad} ve dogum tarihi {DogumTarihi}");
+        }
+        public void OgretmeniYazdir()
+        {
+            Console.WriteLine($"Ogretmenin adi {Ad} {Soyad} ve dogum tarihi {DogumTarihi}");
+        }
+    }
+}
---
+++ PatikaW3P1_IlkSinifimiziOlusturuyoruz/Program.cs
@@ -0,0 +1,25 @@
+﻿using PatikaW3P1_IlkSinifimiziOlusturuyoruz;
+
+Person BirinciOgrenci = new Person(); // yeni bir nesne olusturuyoruz BirinciOgrenci adinda 
+BirinciOgrenci.Ad = "Aysel"; //nesnenin icerigi yazilir
+BirinciOgrenci.Soyad = "Bilmez";
+BirinciOgrenci.DogumTarihi = "01.01.1998";
+BirinciOgrenci.OgrenciyiYazdir(); //ogrenci oldugu icin bu metot kullanilir ve  ad soyad ve dogumtarihi konsola yazilir
+
+Person BirinciOgretmen = new Person();
+BirinciOgretmen.Ad = "Ozcan";
+BirinciOgretmen.Soyad = "Yazici";
+BirinciOgretmen.DogumTarihi = "01.01.1975";
+BirinciOgretmen.OgretmeniYazdir();
+
+Person IkinciOgrenci = new Person();
+IkinciOgrenci.Ad = "Esra";
+IkinciOgrenci.Soyad = "Suluoglu";
+IkinciOgrenci.DogumTarihi = "02.02.1999";
+IkinciOgrenci.OgrenciyiYazdir();
+
+Person IkinciOgretmen = new Person();
+IkinciOgretmen.Ad = "Zeliha";
+IkinciOgretmen.Soyad = "Al";
+IkinciOgretmen.DogumTarihi = "02.02.1970";
+IkinciOgretmen.OgretmeniYazdir();
